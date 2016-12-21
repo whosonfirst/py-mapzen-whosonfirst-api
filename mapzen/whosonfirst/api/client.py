470952be983de86e8a546c06b4d6b3101525671c
thisisaaroland <aaron@mapzen.com>
2016-12-21 10:16:32 -0800

    update default endpoint

--- mapzen/whosonfirst/api/client.py
+++ mapzen/whosonfirst/api/client.py
@@ -5,9 +5,9 @@ class OAuth2(flamework.api.client.OAuth2):
     def __init__(self, access_token, **kwargs):
 
         if not kwargs.get('hostname', None):
-            kwargs['hostname'] = 'whosonfirst.mapzen.com'
+            kwargs['hostname'] = 'whosonfirst-api.mapzen.com'
 
         if not kwargs.get('endpoint', None):
-            kwargs['endpoint'] = '/api/rest/'
+            kwargs['endpoint'] = '/'
 
         flamework.api.client.OAuth2.__init__(self, access_token, **kwargs)
