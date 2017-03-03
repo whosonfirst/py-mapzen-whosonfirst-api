bb6102bb4517458ba2e5419e57c2a3b705d2d25a
thisisaaroland <aaron@mapzen.com>
2017-03-02 18:27:13 -0800

    first pass at MZ API client; bump to 0.032

--- mapzen/whosonfirst/api/client.py
+++ mapzen/whosonfirst/api/client.py
@@ -1,5 +1,22 @@
 import flamework.api.client
 
+class Mapzen(flamework.api.client.OAuth2):
+
+    def __init__(self, api_key, **kwargs):
+        
+        if not kwargs.get('hostname', None):
+            kwargs['hostname'] = 'whosonfirst-api.mapzen.com'
+
+        if not kwargs.get('endpoint', None):
+            kwargs['endpoint'] = '/'
+
+        self.api_key = api_key
+        
+        flamework.api.client.OAuth2.__init__(self, None, **kwargs)    
+        
+    def set_auth(self, kwargs):
+        kwargs['api_key'] = self.api_key
+    
 class OAuth2(flamework.api.client.OAuth2):
 
     def __init__(self, access_token, **kwargs):
